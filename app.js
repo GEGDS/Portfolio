844ecfe3d08d107320c1e248e5ccb132dd6d3038
Gabriel Silva <gabrielback2009@hotmail.com>
2024-02-16 22:17:17 -0300

    commit v1 estrutura do projeto

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { router } from './routes/routes.js'; 
 
 const app = express();
 const port = 3000; 
@@ -7,7 +8,8 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('public'));
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ${port}');
+    console.log(`Servidor rodando na porta ${port}`);
 });
