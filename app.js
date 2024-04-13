60bb810bb81d602dd3a7cceb18a4334357102460
Gabriel Silva <gabrielback2009@hotmail.com>
2024-04-13 01:27:35 -0300

    commit v11 atualizando footer e projetos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from 'path';
 import { router } from './routes/routes.js'; 
 
 const app = express();
@@ -7,8 +8,9 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve('public')));
 app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,24 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.render('home.ejs');
+    const projects = [
+        {
+            title: 'Simon Game',
+            image:'/Images/Simon Game.png',
+            alt:'portfolio carousel',
+            description: 'Jogo de gravar a sequencia de cores',
+        }, 
+
+        {
+            title: 'IMC Calculator',
+            image:'/Images/IMC Calculator.png',
+            alt:'portfolio carousel',
+            description: 'calculadora de indice de massa corporal'
+        }
+         
+    ];
+
+    res.render('home', {projects});
 });
 
 
